c6ffe5048c8356c2ab3075e36ed333af3d3e1491
Caio Willian <caiow.willian@gmail.com>
2019-03-31 19:44:51 -0300

    Adicionado métodos assincronos

--- src/Operations/RedisOperation.cs
+++ src/Operations/RedisOperation.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ClientRedis.Operations
 {
@@ -16,12 +17,12 @@ namespace ClientRedis.Operations
         /// </summary>
         /// <param name="obj">objeto que será armazendo</param>
         /// <param name="key"></param>
+        /// <param name="expiry">tempo de expiração</param>
         public static void Add(T obj, string key, TimeSpan? expiry = null)
         {
-            if(expiry == null)
+            if (expiry == null)
             {
                 GetDatabase().StringSet(key, JsonConvert.SerializeObject(obj));
-
                 return;
             }
 
@@ -29,15 +30,49 @@ namespace ClientRedis.Operations
         }
 
         /// <summary>
+        /// Adiciona um dado no redis de forma assincrona.
+        /// </summary>
+        /// <param name="obj">objeto que será adicionado no redis</param>
+        /// <param name="keys">chave desse objeto</param>
+        /// <param name="expiry">tempo de expiração</param>
+        /// <returns></returns>
+        public static async Task AddAsync(T obj, string key, TimeSpan? expiry = null)
+        {
+            if (expiry == null)
+            {
+                await GetDatabase().StringSetAsync(key, JsonConvert.SerializeObject(obj));
+                return;
+            }
+
+            await GetDatabase().StringSetAsync(key, JsonConvert.SerializeObject(obj), expiry);
+        }
+
+        /// <summary>
         /// Adiciona um dado no redis
         /// </summary>
         /// <param name="obj"></param>
         /// <param name="keys"></param>
+        /// <param name="expiry">tempo de expiração</param>
         public static void Add(T obj, IList<string> keys, TimeSpan? expiry = null)
         {
             foreach(var key in keys)
             {
                 Add(obj, key, expiry);
+            }
+        }
+
+        /// <summary>
+        /// Adiciona um dado no redis de forma assincrona.
+        /// </summary>
+        /// <param name="obj">objeto que será adicionado no redis</param>
+        /// <param name="keys">chaves desse objeto</param>
+        /// <param name="expiry">tempo de expiração</param>
+        /// <returns></returns>
+        public static async Task AddAsync(T obj, IList<string> keys, TimeSpan? expiry = null)
+        {
+            foreach(var key in keys)
+            {
+                await AddAsync(obj, key, expiry);
             }
         }
 
@@ -51,6 +86,16 @@ namespace ClientRedis.Operations
             return JsonConvert.DeserializeObject<T>(GetDatabase().StringGet(key));
         }
 
+        /// <summary>
+        /// Obtém um objeto salvo no redis de forma assincrona
+        /// </summary>
+        /// <param name="key">chave do registro</param>
+        /// <returns></returns>
+        public static async Task<T> GetAsync(string key)
+        {
+            return JsonConvert.DeserializeObject<T>(await GetDatabase().StringGetAsync(key));
+        }
+        
         /// <summary>
         /// Obtém o redis
         /// </summary>
