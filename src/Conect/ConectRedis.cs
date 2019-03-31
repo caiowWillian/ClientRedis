288b29ae2d828096ea48da1fa7b36ad31f0efe7c
Caio Willian <caiow.willian@gmail.com>
2019-03-31 18:56:41 -0300

    Primeiro Commit

---
+++ src/Conect/ConectRedis.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace ClientRedis.Conect
+{
+    public class ConectRedis
+    {
+        static ConectRedis()
+        {
+            lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect("localhost:6379");
+            });
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
---
+++ src/Operations/RedisOperation.cs
@@ -0,0 +1,66 @@
+﻿using ClientRedis.Conect;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClientRedis.Operations
+{
+    public class RedisOperation<T> 
+        where T : class
+    {
+        protected static IDatabase _database;
+
+        /// <summary>
+        /// Adiciona um dado no redis
+        /// </summary>
+        /// <param name="obj">objeto que será armazendo</param>
+        /// <param name="key"></param>
+        public static void Add(T obj, string key)
+        {
+            var cache = ConectRedis.Connection.GetDatabase();
+            GetDatabase().StringSet(key, JsonConvert.SerializeObject(obj));
+        }
+
+        /// <summary>
+        /// Adiciona um dado no redis
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="keys"></param>
+        public static void Add(T obj, IList<string> keys)
+        {
+            var cache = ConectRedis.Connection.GetDatabase();
+
+            foreach(var key in keys)
+            {
+                GetDatabase().StringSet(key, JsonConvert.SerializeObject(obj));
+            }
+        }
+
+        /// <summary>
+        /// Obtém um objeto salvo no redis
+        /// </summary>
+        /// <param name="key">chave do registro</param>
+        /// <returns></returns>
+        public static T Get(string key)
+        {
+            return JsonConvert.DeserializeObject<T>(GetDatabase().StringGet(key));
+        }
+
+        /// <summary>
+        /// Obtém o redis
+        /// </summary>
+        /// <returns></returns>
+        protected static IDatabase GetDatabase()
+        {
+            if(_database != null)
+            {
+                return _database;
+            }
+
+            _database = ConectRedis.Connection.GetDatabase();
+
+            return _database;
+        }
+    }
+}
